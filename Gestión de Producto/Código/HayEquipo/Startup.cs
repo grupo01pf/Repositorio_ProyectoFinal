75b11bdcb43d4456e64bb6223b67308bba17c29d
Grupo 01 - Proyecto Final <grupo01pf@gmail.com>
2019-07-31 14:21:41 -0300

    Código base del proyecto

---
+++ Gestión de Producto/Código/HayEquipo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HayEquipo.Startup))]
+namespace HayEquipo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
